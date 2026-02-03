398ddaa4cecf48dbbc94f7a0b189d13a5fb6963d
Fiona <24011183@myrp.edu.sg>
2026-02-03 15:27:50 +0800

    Added lint to CI workflow

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+
 // for the frontend files
 app.use(express.static("public"));
 
-// This will act as our simple "database"
+// "database"
 let todos = [];
 
 
-// CREATE - Add a new to-do
+// CREATE
 app.post("/todos", (req, res) => {
   const { title } = req.body;
   if (!title) return res.status(400).json({ error: "Title is required" });
@@ -22,12 +23,12 @@ app.post("/todos", (req, res) => {
   res.status(201).json(newTodo);
 });
 
-// READ - Get all to-dos
+// READ
 app.get("/todos", (req, res) => {
   res.json(todos);
 });
 
-// UPDATE - Toggle a to-do’s completed status
+// UPDATE
 app.put("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const todo = todos.find((item) => item.id === id);
@@ -38,7 +39,7 @@ app.put("/todos/:id", (req, res) => {
   res.json(todo);
 });
 
-// DELETE - Remove a to-do
+// DELETE
 app.delete("/todos/:id", (req, res) => {
   const id = parseInt(req.params.id);
   todos = todos.filter((item) => item.id !== id);
